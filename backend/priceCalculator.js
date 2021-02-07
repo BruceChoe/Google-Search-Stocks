0da5295d918681cbadc16a30e74530087126d46b
Bruce Choe <bruceleechoe@gmail.com>
2021-02-06 21:02:36 -0800

    DirectPriceCalculator Class WIP
    Created class and constructor

--- backend/priceCalculator.js
+++ backend/priceCalculator.js
@@ -7,4 +7,25 @@ class PriceCalculator {
 // TODO @Bruce: Create a "DirectPriceCalculator" that calculates the price of a term based on only that term's
 // popularity, not on other terms related to it.
 //test push
-// class DirectPriceCalculator {}
+
+const x = 1;
+myDate = new Date();
+console.log(myDate);
+myDate.setDate(myDate.getDate() - 1);
+console.log(myDate);
+
+class DirectPriceCalculator {
+    constructor(searchTerm) {
+        this.currentDate = new Date();
+        this.searchTerm = searchTerm;
+        this.volume = webMasterAPI(searchTerm, currentDate - 1, currentDate);
+    }
+
+    get volume() {
+        //return webMasterAPI(this.searchTerm,
+    }
+
+    
+
+   
+}
