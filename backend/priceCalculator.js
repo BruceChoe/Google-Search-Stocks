c15b808e442802cc278d39eefc6deaf7affabf77
grchristensen <grchr4000@gmail.com>
2021-02-06 18:49:58 -0800

    Add price calculator interface

---
+++ backend/priceCalculator.js
@@ -0,0 +1,10 @@
+class PriceCalculator {
+  calculate(term) {
+    return 50.
+  }
+}
+
+// TODO @Bruce: Create a "DirectPriceCalculator" that calculates the price of a term based on only that term's
+// popularity, not on other terms related to it.
+
+// class DirectPriceCalculator {}
