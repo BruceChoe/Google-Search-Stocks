549d82a9a1b9ed0d7457ceef66e704d0530908cb
Bruce Choe <bruceleechoe@gmail.com>
2021-02-13 18:59:08 -0800

    Create basic function structure

--- backend/market.js
+++ backend/market.js
@@ -1,1 +1,17 @@
-//git test
+/*
+ * Market Class File
+*/
+
+class Market {
+    constructor(priceCalculator) {
+        this.priceCalculator = priceCalculator;
+    }
+
+    buy(searchTerm, amount, inventory) {
+        //TODO
+    }
+
+    sell(searchTerm, amount, inventory) {
+        //TODO
+    }
+}
