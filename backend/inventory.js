3834a7312c4af72aab938caec7cd5954a6173751
Bruce Choe <bruceleechoe@gmail.com>
2021-02-20 15:40:35 -0800

    Update addShare and removeShare

--- backend/inventory.js
+++ backend/inventory.js
@@ -4,21 +4,28 @@
 
 class inventory {
     constructor() {
-        this.money = 100, 000, 000;
+        this.money = 100000000;
     }
 
     addShare(searchTerm, amount) {
         if (stocks[searchTerm] === undefined) {
             stocks[searchTerm] = amount; 
         } else {
-            stocks[searchTerm] = amount; 
+            stocks[searchTerm] = stocks[searchTerm] + amount; 
         }
-           // term does exist
     }
 
-    removeShare
+    removeShare(searchTerm, amount) {
+        if (stocks[searchTerm] === undefined) {
+            // If share does not exist, throw error code
+        } else {
+            stocks[searchTerm] = stocks[searchTerm] - amount; //TODO: If amount exceeds current amount of shares, 
+        }
+    }
 }
 
 stocks = new Object();
 stocks["h"] = 1;
-console.log(stocks["hi"]);
+console.log(stocks["h"]);
+stocks["h"] = stocks["h"] + 1;
+console.log(stocks["h"]);
